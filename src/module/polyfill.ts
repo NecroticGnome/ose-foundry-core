51a56307c81c5db4a74f6f1b6231a6bb3730afec
JustinFreitas <justin.s.freitas@me.com>
2025-08-03 07:48:08 -0600

    Unsure where Math.clamped was defined, so instead, let's inline an implementation.

--- src/module/polyfill.ts
+++ src/module/polyfill.ts
@@ -6,6 +6,8 @@ export default () => {
   // @ts-ignore - Math.clamp is not a standard JS function
   if (typeof Math.clamp !== "function") {
     // @ts-ignore - Math.clamp is not a standard JS function
-    Math.clamp = Math.clamped;
+    Math.clamp = function(value: number, min: number, max: number): number {
+      return Math.min(Math.max(value, min), max);
+    };
   }
 };
