814d9567fca441201c873fa85f0d1c1301b1f510
Blair McMillan <blair@mcmillan.id.au>
2026-01-19 14:47:52 +1000

    chore: convert away from jquery syntax

--- src/module/item/item-sheet.js
+++ src/module/item/item-sheet.js
@@ -9,8 +9,7 @@ import OSE from "../config";
 export default class OseItemSheet extends foundry.appv1.sheets.ItemSheet {
   /**
    * Extend and override the default options used by the Simple Item Sheet
-   *
-   * @returns {object}
+   * @returns {object} Options for rendering the sheet
    */
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
@@ -39,7 +38,6 @@ export default class OseItemSheet extends foundry.appv1.sheets.ItemSheet {
   /**
    * Prepare data for rendering the Item sheet
    * The prepared data object contains both the actor data as well as additional sheet options
-   *
    * @returns {object} Data for the Handlebars template
    */
   async getData() {
@@ -50,10 +48,11 @@ export default class OseItemSheet extends foundry.appv1.sheets.ItemSheet {
       encumbrance: game.settings.get(game.system.id, "encumbranceOption"),
     };
     data.enriched = {
-      description: await foundry.applications.ux.TextEditor.implementation.enrichHTML(
-        this.item.system?.description || "",
-        { async: true }
-      ),
+      description:
+        await foundry.applications.ux.TextEditor.implementation.enrichHTML(
+          this.item.system?.description || "",
+          { async: true }
+        ),
     };
     return data;
   }
@@ -62,29 +61,45 @@ export default class OseItemSheet extends foundry.appv1.sheets.ItemSheet {
 
   /**
    * Activate event listeners using the prepared sheet HTML
-   *
    * @param {JQuery} html - The prepared HTML object ready to be rendered into the DOM
    */
   activateListeners(html) {
-    html.find('input[data-action="add-tag"]').keypress((ev) => {
-      if (ev.which === 13) {
-        const value = $(ev.currentTarget).val();
-        const values = value.split(",");
-        this.object.pushManualTag(values);
-      }
-    });
-    html.find(".tag-delete").click((ev) => {
-      const value = ev.currentTarget.parentElement.dataset.tag;
-      this.object.popManualTag(value);
-    });
-    html.find("a.melee-toggle").click(() => {
-      this.object.update({ "system.melee": !this.object.system.melee });
-    });
-
-    html.find("a.missile-toggle").click(() => {
-      this.object.update({ "system.missile": !this.object.system.missile });
-    });
-
     super.activateListeners(html);
+    html = html instanceof HTMLElement ? html : html[0];
+
+    html
+      .querySelector('input[data-action="add-tag"]')
+      ?.addEventListener("keydown", (ev) => {
+        if (ev.key !== "Enter") return;
+
+        const value = ev.currentTarget?.value ?? "";
+        const values = value
+          .split(",")
+          .map((v) => v.trim())
+          .filter(Boolean);
+
+        if (values.length === 0) return;
+        this.object.pushManualTag(values);
+      });
+
+    html.querySelectorAll(".tag-delete").forEach((el) => {
+      el.addEventListener("click", (ev) => {
+        const tag = ev.currentTarget?.parentElement?.dataset?.tag;
+        if (!tag) return;
+        this.object.popManualTag(tag);
+      });
+    });
+
+    html.querySelectorAll("a.melee-toggle").forEach((el) => {
+      el.addEventListener("click", () => {
+        this.object.update({ "system.melee": !this.object.system.melee });
+      });
+    });
+
+    html.querySelectorAll("a.missile-toggle").forEach((el) => {
+      el.addEventListener("click", () => {
+        this.object.update({ "system.missile": !this.object.system.missile });
+      });
+    });
   }
 }
